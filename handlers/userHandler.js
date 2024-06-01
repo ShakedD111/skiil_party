a706f5955167e91f6d7203415862f2f4460aef7c
ShakedDror <shakeddror28@gmail.com>
2024-06-01 15:46:23 +0300

    changing file name

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -1,5 +1,5 @@
 //const express = require('express');
-const schema = require('../schemas/usersModel');
+const schema = require('../schemas/usersSchemaModels');
 
 
 const userInfo = async (req, res) => {
